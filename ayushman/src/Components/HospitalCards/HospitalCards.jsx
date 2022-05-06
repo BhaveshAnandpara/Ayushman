3e975697892edd881d78191331c86af2bc72c988
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-05-06 17:55:37 +0530

    Hospital Card Updated

--- ayushman/src/Components/HospitalCards/HospitalCards.jsx
+++ ayushman/src/Components/HospitalCards/HospitalCards.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import '../HospitalCards/HospitalCards.css'
+import bedIcon from '../../Assets/Icons/bed-icon.svg'
 
 export default function HospitalCards() {
   return (
     <div className="card ">
-        <div className="card-img border"></div>
+        <div className="card-img ">
+        </div>
 
         <div className="card-info ">
             <div className='card-hosp-text'>
@@ -12,8 +14,10 @@ export default function HospitalCards() {
                 <p className="card-address">Dummy Road Address, Nagpur, Maharashtra.</p>
             </div>
             <div className='card-info-box-container '>
-                <div className="card-info-box border">
-                    <img src="" alt="bed-icon" />
+                <div className="card-info-box ">
+                    <img className='bed-icon' src={bedIcon} alt="bed-icon" />
+                    <span className='card-info-box-text'>Available Emergency Beds</span>
+                    <p className=' card-info-box-text red' >15</p>
                 </div>
                 <div className="card-info-box border"></div>
                 <div className="card-info-box border"></div>
