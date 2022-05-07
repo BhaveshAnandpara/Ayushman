67ef3cff10dae4e79244b5439982715c6a2be8b9
vaishna2002 <co.2020.vabhende@bitwardha.ac.in>
2022-05-07 15:03:30 +0530

    commit header

--- ayushman/src/App.js
+++ ayushman/src/App.js
@@ -5,11 +5,12 @@ import ApproximateCost from './Pages/Approximate-Cost/Approximate-cost'
 import Feedback from './Pages/Feedback/Feedback'
 import Login from './Pages/Login/Login'
 import HospitalCard from './Components/HospitalCards/HospitalCards'
-
+import Header from './Components/Header/Header';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
         <HospitalCard/>
     </div>
   );
