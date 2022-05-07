e60f80ce94993a8390a651f9397980f607fe7373
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-05-07 12:05:29 +0530

    Hospital Card Done

--- ayushman/src/Components/HospitalCards/HospitalCards.jsx
+++ ayushman/src/Components/HospitalCards/HospitalCards.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import '../HospitalCards/HospitalCards.css'
 import bedIcon from '../../Assets/Icons/bed-icon.svg'
+import oxygenIcon from '../../Assets/Icons/oxygen-icon.svg'
 
 export default function HospitalCards() {
   return (
@@ -14,14 +15,31 @@ export default function HospitalCards() {
                 <p className="card-address">Dummy Road Address, Nagpur, Maharashtra.</p>
             </div>
             <div className='card-info-box-container '>
+
                 <div className="card-info-box ">
                     <img className='bed-icon' src={bedIcon} alt="bed-icon" />
                     <span className='card-info-box-text'>Available Emergency Beds</span>
                     <p className=' card-info-box-text red' >15</p>
                 </div>
-                <div className="card-info-box border"></div>
-                <div className="card-info-box border"></div>
-                <div className="card-info-box border"></div>
+
+                <div className="card-info-box ">
+                    <img className='bed-icon' src={bedIcon} alt="bed-icon" />
+                    <span className='card-info-box-text'>Available Beds with Oxygen</span>
+                    <p className=' card-info-box-text red' >5</p>
+                </div>
+
+                <div className="card-info-box ">
+                    <img className='bed-icon' src={bedIcon} alt="bed-icon" />
+                    <span className='card-info-box-text'>Available Beds with Ventilator</span>
+                    <p className=' card-info-box-text red' >6</p>
+                </div>
+
+                <div className="card-info-box ">
+                    <img className='bed-icon' src={oxygenIcon} alt="oxygen-icon" />
+                    <span className='card-info-box-text'>Oxygen Available</span>
+                    <p className=' card-info-box-text green' >20 Days</p>
+                </div>
+
             </div>
         </div>
 
