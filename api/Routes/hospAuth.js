06142d5fd1d3f0b5f61222e984e409cd63d7e08b
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-06-09 15:31:17 +0530

    Hospital Model Created

---
+++ api/Routes/hospAuth.js
@@ -0,0 +1,17 @@
+const router = require("express").Router()
+const CryptoJS = require("crypto-js")
+const dotenv = require("dotenv")
+dotenv.config()
+const jwt = require('jsonwebtoken')
+
+const JWT_AUTH_TOKEN = process.env.JWT_AUTH_TOKEN
+const JWT_REFRESH_TOKEN = process.env.JWT_REFRESH_TOKEN
+const crypto = require('crypto') //For Hashing
+
+const smsKey = process.env.SMS_SECRET_KEY
+
+router.post('/hospRegister' , async(req,res)=>{
+
+    
+
+})
