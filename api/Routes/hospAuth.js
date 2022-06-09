13e686b34dcdd23f80d0cdd2f0c162de4c9ecc39
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-06-09 15:58:29 +0530

    Hospital Login Authentication Done

--- api/Routes/hospAuth.js
+++ api/Routes/hospAuth.js
@@ -11,6 +11,8 @@ const crypto = require('crypto') //For Hashing
 
 const smsKey = process.env.SMS_SECRET_KEY
 
+
+
 router.post('/hospLogin', async (req, res) => {
 
     const hospID = req.body.hospID
@@ -18,13 +20,21 @@ router.post('/hospLogin', async (req, res) => {
 
     try {
         const hospital = await Hospital.findOne({ Hosp_id: hospID }) //Find hospital in Database by PhoneNo
+        const hospPassword = hospital.Hosp_Password
+
+        // var bytes = CryptoJS.AES.decrypt(hospital.Hosp_Password, process.env.SECRET_KEY); //Decrypt the Encrypted Password
+        // var originalPass = bytes.toString(CryptoJS.enc.Utf8); //Original Password
 
 
-        var bytes = CryptoJS.AES.decrypt(hospital.Hosp_Password, process.env.SECRET_KEY); //Decrypt the Encrypted Password
-        var originalPass = bytes.toString(CryptoJS.enc.Utf8); //Original Password
-
-        originalPass !== password &&
+        if( hospPassword !== password ){
             res.status(401).json("Invalid Password")
+        }
+        else{
+            res.status(401).json("Login Confirmed")
+        }
+
+
+
     } catch (err) {
         res.json("Invalid Login")
     }
