9d3db5cf2c119958295d13427ea838bc3829594f
Pranjal Panchawate <pranjalpanchawate@gmail.com>
2022-07-05 17:37:53 +0530

    Login page updated

--- ayushman/src/Pages/Login/Login.jsx
+++ ayushman/src/Pages/Login/Login.jsx
@@ -1,40 +1,48 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 import '../Login/Login.css'
 
-export default function Login() {
+export default function Login( props ) {
   return (
 
-    <div className='login-main'>
-      <div className='login-items'>
-        <span> Patient </span>
-        <span> Hospital </span>
-        <span> Medical Council </span>
+    <section className='login-main '>
+
+      <div className='login-items '>
+        <Link to={`/hospitalDashboard/${props.loginItem}`} className={`  ${ props.loginItem=="Patient" ? " active-loginItem" : "" } `}  > Patient </Link>
+        <Link to={`/hospitalDashboard/${props.loginItem}`} className={`  ${ props.loginItem=="Hospital" ? " active-loginItem" : "" } `}  > Hospital </Link>
+        <Link to={`/hospitalDashboard/${props.loginItem}`} className={`  ${ props.loginItem=="Medical Council" ? " active-loginItem" : "" } `}  > Medical Council </Link>
 
       </div>
 
-      <div className='login-container'>
+      <div className='login-conntent-container border'>
 
 
-        <div className='patient-container'>
-          <div className='patient-container-1'>
-            <input type="number" placeholder="+91 | Mobile No." />
-            <button type="button">Get OTP</button>
+
+        <div className='login-container'>
+
+
+          <div className='patient-container'>
+            <div className='patient-container-1'>
+              <input type="number" placeholder="+91 | Mobile No." />
+              <button type="button">Get OTP</button>
+            </div>
+            <div><p>OTP has been sent to above number</p></div>
+            <div className='patient-container-2'>
+
+              <div className='otp1'><input type="number" /></div>
+              <div className='otp2'><input type="number" /></div>
+              <div className='otp3'><input type="number" /></div>
+              <div className='otp4'><input type="number" /></div>
+            </div>
+            <div>
+              <button type="button">LOGIN</button>
+            </div>
+
           </div>
-          <div><p>OTP has been sent to above number</p></div>
-          <div className='patient-container-2'>
-
-            <div className='otp1'><input type="number" /></div>
-            <div className='otp2'><input type="number" /></div>
-            <div className='otp3'><input type="number" /></div>
-            <div className='otp4'><input type="number" /></div>
-          </div>
-          <div>
-            <button type="button">LOGIN</button>
-          </div>
-
         </div>
+
       </div>
-      <div className='hospital-login-container border'>
+      {/* <div className='hospital-login-container border'>
         <div className='hospital-id'>
           <input type="text" placeholder='Hospital ID'></input>
         </div>
@@ -57,7 +65,7 @@ export default function Login() {
 
 
 
-      </div>
-    </div>
+      </div> */}
+    </section>
   )
 }
