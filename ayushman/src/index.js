23b87101bcae51d4000fd632d47fb176a4f1d562
Bhavesh Anandpara <bhaveshAP09112002@gmail.com>
2022-08-04 10:30:36 +0530

    Added redux-persist to store State

--- ayushman/src/index.js
+++ ayushman/src/index.js
@@ -3,18 +3,33 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {applyMiddleware, createStore, compose} from 'redux'
+import { applyMiddleware, createStore, compose } from 'redux'
 import allReducers from './States/reducers';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import { PersistGate } from 'redux-persist/integration/react'
+
+const persistConfig = {
+        key: 'root',
+        storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, allReducers)
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(allReducers, composeEnhancer(applyMiddleware(thunk)) )
+const store = createStore(persistedReducer, composeEnhancer(applyMiddleware(thunk)))
+let persistor = persistStore(store)
+
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
         <Provider store={store}>
-        <App />
+                <PersistGate loading={null} persistor={persistor}>
+                        <App />
+                </PersistGate>
         </Provider>
 );
 
